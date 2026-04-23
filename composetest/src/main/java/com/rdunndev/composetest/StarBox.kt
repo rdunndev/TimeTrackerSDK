732f6d3fe55f6981ea50d8d133f14b521f8e1b9d
rdunndev <rdunndev@gmail.com>
2026-04-23 15:28:29 +0100

    Make star box sizeable

--- composetest/src/main/java/com/rdunndev/composetest/StarBox.kt
+++ composetest/src/main/java/com/rdunndev/composetest/StarBox.kt
@@ -2,13 +2,18 @@ package com.rdunndev.composetest
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.Dp
 
 @Composable
-fun StartBox() {
-    Box(modifier = Modifier) {
+fun StartBox(size: Dp) {
+    Box(modifier = Modifier.height(size)
+        .width(size)
+    ) {
         Image(painter = painterResource(R.drawable.star), contentDescription = "")
     }
 }
